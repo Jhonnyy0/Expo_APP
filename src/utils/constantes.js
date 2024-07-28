824b3132278a08b890cbb35a83a5b5c33d565abd
Jhonnyy0 <136754303+Jhonnyy0@users.noreply.github.com>
2024-07-28 11:43:02 -0600

    Arreglo de archivos innecesarios

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,9 +1,3 @@
-
-//IP de donde se encuentra la Api, en este caso la dirección 
-//Ip de la maquina local 
-//utilizar la direccion IP del servidor y no localhost
-//export const IP = 'http://10.10.0.165';
-//export const IP = 'http://192.168.137.65';
+// IP de donde se encuentra la Api, en este caso la dirección 
+// Ejecutar comando ipconfig en CMD para obtener direccion ip
 export const IP = 'http://192.168.1.13';
-//export const IP = 'http://172.20.10.10';
-
