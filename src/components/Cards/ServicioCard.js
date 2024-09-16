a60802d6c98808fd8819b9d2030d5876a2a828b8
Jhonny Amaya <136754303+Jhonnyy0@users.noreply.github.com>
2024-09-16 14:49:28 -0600

    Creacion de citas por servicios

---
+++ src/components/Cards/ServicioCard.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { Text, TouchableOpacity, View, StyleSheet, ImageBackground, Alert } from 'react-native';
+import Constants from 'expo-constants';
+import * as Constantes from '../../utils/constantes'
+
+const ServicioCard = ({ item, getDetalleServicio, navigation }) => {
+
+    const ip = Constantes.IP;
+
+    const handleCreateCita = async (id_servicio) => {
+        try {
+            // Mostrar un mensaje de confirmación antes de eliminar
+            Alert.alert(
+                'Confirmación',
+                '¿Deseas agendar una cita?',
+                [
+                    {
+                        text: 'Cancelar',
+                        style: 'cancel'
+                    },
+                    {
+                        text: 'Agregar',
+                        onPress: async () => {
+                            const formData = new FormData();
+                            formData.append('idServicio', id_servicio);
+                            const response = await fetch(`${ip}/expo_2024_v2/api/services/public/cita.php?action=createRowCliente`, {
+                                method: 'POST',
+                                body: formData
+                            });
+                            const data = await response.json();
+                            if (data.status) {
+                                Alert.alert('Cita agendada correctamente');
+                                // Llamar a la función de actualización para actualizar la lista
+                                getDetalleServicio(prevData => prevData.filter(item => item.id_servicio !== id_servicio));
+                            } else {
+                                Alert.alert('Error al momento de agendar cita', data.error);
+                            }
+                        }
+                    }
+                ]
+            );
+        } catch (error) {
+            Alert.alert("Error al agendar cita")
+        }
+    };
+
+    return (
+        <View style={styles.itemContainer}>
+            <TouchableOpacity onPress={() => handleCreateCita(item.id_servicio)}
+                activeOpacity={0.5}>
+                <Text style={styles.botonTexto}>{item.tipo_servicio}</Text>
+                <Text style={styles.BotonDesc}>{item.descripcion_servicio}</Text>
+                <ImageBackground
+                    source={{ uri: `${ip}/expo_2024_v2/api/images/servicios/${item.imagen_servicio}` }}
+                    style={styles.boton}>
+                </ImageBackground>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+export default ServicioCard;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#EAD8C0',
+        paddingTop: Constants.statusBarHeight,
+        paddingHorizontal: 16,
+    },
+    itemContainer: {
+        padding: 16,
+        marginVertical: 8,
+        marginHorizontal: 6,
+        backgroundColor: '#000',
+        borderRadius: 8,
+        shadowColor: '#0c84e4',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 4,
+    },
+    boton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 350,
+        height: 300,
+        marginBottom: 20,
+        opacity: 0.7,
+    },
+    botonTexto: {
+        fontFamily: 'monospace',
+        fontSize: 21,
+        color: '#026AA7',
+        fontWeight: 'bold',
+    },
+    BotonDesc: {
+        alignContent: 'center',
+        fontFamily: 'monospace',
+        fontSize: 16,
+        color: 'white',
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+});
--- src/screens/Quiropractica.js
+++ src/screens/Quiropractica.js
@@ -1,24 +1,72 @@
-import { StatusBar, StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, FlatList, ScrollView, SafeAreaView, Image, Modal } from 'react-native';
-import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Alert, FlatList } from 'react-native';
+import React, { useState } from 'react';
 import * as Constantes from '../utils/constantes'
-import Buttons from '../components/Buttons/Button';
-import RNPickerSelect from 'react-native-picker-select';
+import { useFocusEffect } from '@react-navigation/native';
 import Constants from 'expo-constants';
-import { FontAwesome } from '@expo/vector-icons'; // Importamos el ícono
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import ServicioCard from '../components/Cards/ServicioCard';
 
 export default function Quiropractica({ navigation }) {
+    // Estado para almacenar los detalles del historial
+    const [dataDetalleServicios, setdataDetalleServicios] = useState([]);
+    // Estado para el id del detalle seleccionado para modificar
+    const [idServicio, setidServicio] = useState(null);
+    // IP del servidor
+    const ip = Constantes.IP;
 
-    const volverInicio = async () => {
-        navigation.navigate('Home');
+    // Efecto para cargar los detalles del historial al cargar la pantalla o al enfocarse en ella
+    useFocusEffect(
+        // La función useFocusEffect ejecuta un efecto cada vez que la pantalla se enfoca.
+        React.useCallback(() => {
+            getDetalleServicio(); // Llama a la función getDetalleServicio.
+        }, [])
+    );
+
+    // Función para obtener los detalles del historial desde el servidor
+    const getDetalleServicio = async () => {
+        try {
+            const response = await fetch(`${ip}/expo_2024_v2/api/services/public/servicio.php?action=readAll8`, {
+                method: 'GET',
+            });
+            const data = await response.json();
+            console.log(data, "Data desde getServicio")
+            if (data.status) {
+                setdataDetalleServicios(data.dataset);
+            } else {
+                console.log("No hay detalles de Servicios disponibles")
+            }
+        } catch (error) {
+            console.error(error, "Error desde Catch");
+            Alert.alert('Error', 'Ocurrió un error al listar la agenda');
+        }
     };
+
+    // Función para renderizar cada elemento de la agenda
+    const renderItem = ({ item }) => (
+        <ServicioCard
+            item={item}
+            idServicio={idServicio}
+            setidServicio={setidServicio}
+            getDetalleServicio={getDetalleServicio}
+        />
+    );
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Quiropractica Especifica</Text>
-            <Buttons
-                textoBoton='Volver a Home'
-                accionBoton={volverInicio}
-            />
+            <Text style={styles.title}>Servicios disponibles <FontAwesome name="user-md" size={24} color="#fff" style={styles.icon} /></Text>
+            <Text style={styles.subtitulo}>Haz click en un servicio de preferencia para solicitar una cita.</Text>
+            <View style={{ alignItems: 'center' }}>
+                <FontAwesome name="caret-down" size={24} color="#fff" style={styles.icon} />
+            </View>
+            {dataDetalleServicios.length > 0 ? (
+                <FlatList
+                    data={dataDetalleServicios}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.id_servicio.toString()}
+                />
+            ) : (
+                <Text style={styles.titleDetalle}>No hay servicios disponibles.</Text>
+            )}
         </View>
     );
 }
@@ -38,6 +86,15 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         color: '#FFF',
         fontWeight: '900',
-        fontSize: 24
+        fontSize: 22
     },
+    subtitulo: {
+        textAlign: 'center',
+        fontFamily: 'monospace',
+        marginBottom: 10,
+        marginHorizontal: 15,
+        color: '#757575',
+        fontWeight: '500',
+        fontSize: 16
+    }
 });
