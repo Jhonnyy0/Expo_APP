2c462c888fc04a5701190c19414acfb841693c13
jhonnyy0 <jhonnyamaya168@gmail.com>
2024-09-23 10:38:10 -0600

    Recuperacion de contraseña

--- src/screens/Recuperacion.js
+++ src/screens/Recuperacion.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import { StyleSheet, View, Image, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Image, Text, Alert, TouchableOpacity } from 'react-native';
 import Input from '../components/Inputs/Input'
 import Boton2 from '../components/Buttons/Button2';
 import { Ionicons } from '@expo/vector-icons';
+import * as Constantes from '../utils/constantes'
+import Codigo from './Codigo';
 
 export default function Recuperacion({ navigation }) {
 
-    const volverInicio = () => {
-        navigation.navigate('Codigo');
+    const ip = Constantes.IP;
+
+    const [codigo, setCodigo] = useState('');
+
+    const VerifyCodigo = async () => {
+        try {
+            console.log("Datos a enviar", codigo);
+
+            // Validar que el campo de codigo no esté vacío
+            if (!codigo.trim()) {
+                Alert.alert("Debes ingresar un codigo electrónico");
+                return;
+            }
+
+            // Crear el FormData para enviar el codigo
+            const formData = new FormData();
+            formData.append('codigoUsuario', codigo);
+
+            // Realizar la petición para verificar el codigo
+            const response = await fetch(`${ip}/expo_2024_v2/api/services/public/cliente.php?action=checkUserCodigo`, {
+                method: 'POST',
+                body: formData
+            });
+
+            const data = await response.json();
+            console.log(data, "Data desde enviar codigo OK");
+
+            if (data.status) {
+                console.log(data, 'Valor de enviar codigo OK');
+                navigation.navigate('Contraseña');
+
+                // Aquí puedes llamar a la función para enviar el codigo
+                enviarcodigo(formData);
+            } else {
+                Alert.alert('Error', data.error);
+            }
+        } catch (error) {
+            console.error('Error al intentar verificar el codigo:', error);
+            Alert.alert('Ocurrió un error al intentar leer el codigo');
+        }
     };
 
-    const handleNavigateToPass = () => {
-        navigation.navigate('Contraseña');
+    const volverInicio = () => {
+        navigation.navigate('Codigo');
     };
 
     return (
@@ -26,18 +66,17 @@ export default function Recuperacion({ navigation }) {
             <Text style={styles.texto}>CODIGO DE RECUPERACIÓN</Text>
             <Text style={styles.subtitulo}>Ingresa el codigo que hemos enviado a tu correo electronico, no lo compartas con nadie.</Text>
             <View style={styles.containerInput}>
-                <Input setEditable={true} inputStyle={{ width: 60 }} />
-                <View style={styles.space} />
-                <Input setEditable={true} inputStyle={{ width: 60 }} />
-                <View style={styles.space} />
-                <Input setEditable={true} inputStyle={{ width: 60 }} />
-                <View style={styles.space} />
-                <Input setEditable={true} inputStyle={{ width: 60 }} />
+                <Input
+                    setEditable={true}
+                    setValor={codigo}
+                    setTextChange={setCodigo}
+                    inputStyle={{ width: 250 }}
+                />
             </View>
             <Boton2
                 mode="contained"
                 textoBoton='Aceptar'
-                accionBoton={handleNavigateToPass}
+                accionBoton={VerifyCodigo}
             />
         </View>
     );
@@ -48,7 +87,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'flex-start',
         alignItems: 'center',
-        padding: 20,
+        padding: 10,
         backgroundColor: '#151515',
     },
     backButton: {
--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,3 +1,3 @@
 // IP de donde se encuentra la Api, en este caso la dirección 
 // Ejecutar comando ipconfig en CMD para obtener direccion ip
-export const IP = 'http://10.10.4.175';
+export const IP = 'http://192.168.88.188';
